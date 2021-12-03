39385d54e38afc6a2a17fa5b4a21021faedaeedd
Gabriel Lindman <gabriel.lindman@volvocars.com>
2021-12-03 18:12:02 +0100

    1st assignment of 1st day completed adn approved

--- 1st/sonarSweep.js
+++ 1st/sonarSweep.js
@@ -1,8 +1,24 @@
 var fs = require('fs');
 
 try {  
-    const input = fs.readFileSync('input.txt', 'utf8');
-    console.log('input.txt read.');    
+    var data = fs.readFileSync('input.txt', 'utf8'); //read inputfile
+    var data = data.split("\n"); // split string of data at "\n" (char for new line)
+    console.log('input.txt file read');    
 } catch(e) {
     console.log('Error:', e.stack);
 }
+
+//console.log(parseInt(data[1]))
+var counter = 0;
+for (var i = 0; i < data.length; i++) {
+    //var counter = 0;
+    var tmp1 = parseInt(data[i]) //str to int
+    var tmp2 = parseInt(data[i+1])
+    if (tmp2 > tmp1) {
+        var counter = counter + 1
+    }
+    
+    
+}
+
+console.log(counter);
