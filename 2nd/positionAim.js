d3bf2548188ecf6284b2bdcfb6ef3815f4de4ad9
Gabriel Lindman <gabriel.lindman@volvocars.com>
2021-12-05 20:04:34 +0100

    day 2 task 2 done

---
+++ 2nd/positionAim.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+
+var xlocation = 0
+var depth = 0
+var aim = 0
+
+try {  
+    var data = fs.readFileSync('input.txt', 'utf8'); //read inputfile
+    var data = data.split('\n'); // split string of data at "\n" (char for new line)
+console.log('input.txt file read');    
+} 
+
+catch(e) {
+    console.log('Error:', e.stack);
+}
+var pos_list = []
+var counter = 0
+for (let i = 0; i < data.length; i++) {
+    var tmp = data[i].split(' ') //split each data at ' '
+    pos_list.push(tmp) //add to pos_list
+    counter = counter+1
+  }
+
+pos_list.forEach(function(item) {
+    if (item[0] == 'forward') {
+        xlocation += parseInt(item[1]) //add value to each variable
+        depth += parseInt(item[1])*parseInt(aim)
+    }
+    if (item[0] == 'up') {
+        aim = aim - parseInt(item[1])
+        
+    }
+    if (item[0] == 'down') {
+        aim += parseInt(item[1])
+    }
+
+})
+
+console.log('xlocation * depth: ', xlocation*depth)
