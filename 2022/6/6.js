86a0bc27733408b35a2c84f01360893bb7731d4a
Gabriel Lindman <gabriel.lindman@volvocars.com>
2022-12-11 21:41:54 -0500

    working on 7th

--- 2022/6/6.js
+++ 2022/6/6.js
@@ -6,10 +6,8 @@ const text = readFile(file).split('');
 const check = (data) => {
     for (i=13; i<data.length; i++) {
         const tmp = [data[i],data[i-1],data[i-2],data[i-3],data[i-4],data[i-5],data[i-6],data[i-7],data[i-8],data[i-9],data[i-10],data[i-11],data[i-12],data[i-13]]
-        console.log(tmp)
         const unique = [...new Set(tmp)]
         if (unique.length > 13) {
-            console.log(i+1)
             return
         }
     }
---
+++ 2022/7/7.js
@@ -0,0 +1,80 @@
+const {readFile} = require('../../utils/utils')
+const file = __dirname + '\\test.txt'
+
+const text = readFile(file).split(/\r?\n/)
+const data = []
+text.map((x)=>{data.push(x.split(' '))})
+// bigObj to hold size and files/dirs in each directory
+let bigObj = {
+    '/': []
+}
+
+const fill = (data) => {
+
+    let dirFlag = false
+    let currentDir = ['/']
+    let dirLevel = 0
+    for (i=0;i<data.length;i++) {
+        // if listing dirs
+        if (data[i][0] === '$' && data[i][1] === 'ls') {
+            if (data[i-1][2] != '/') {
+                
+            }
+        dirFlag = false
+        }
+        // if going down a level
+        if (data[i][0] === '$' && data[i][1] === 'cd' && data[i][2] != '/' && data[i][2] != '..') {
+            currentDir.push(data[i][2]) 
+            dirFlag = false
+            /* console.log(data[i][2]) */
+            if (data[i][1] === 'cd') {
+                if (data[i][2] != '/' && data[i][2] != '..') {
+                    if (bigObj[data[i][2]] === undefined) {
+                        bigObj[data[i][2]] = []
+                    }
+                }
+            } 
+        }
+        // if going up a level
+        if (data[i][0] === '$' && data[i][1] === 'cd' && data[i][2] === '..') {
+            currentDir.pop() 
+            dirFlag = false
+        }
+        // all of this stuff is "dir content, lets add it to the object with directories"
+        if (data[i][0] != '$' ) {
+            dirFlag = true
+            const activeDir = currentDir[currentDir.length-1]
+            const pushList = [data[i], currentDir.length]
+            bigObj[activeDir].push(pushList)
+        }
+    }
+}
+
+fill(data)
+
+console.log(bigObj)
+
+// recursive function to give us sizes
+const dirSize = (dir) => {
+    const list = bigObj[dir]
+    let stringList = []
+    let size = 0
+    // loop
+    list.map((x)=>{
+        // is this a lowest level directory? if no recurse over lower directory
+        if (x[0][0] === 'dir') {
+            size += dirSize(x[0][1])
+        }
+        // what files are in the dir? add their size
+        if (Number(x[0][0])) {
+            size += Number(x[0][0])
+        }
+    })
+    return size
+}
+
+for (const x in bigObj) {
+    const tmp = bigObj[x]
+    
+    console.log(x, dirSize(x))
+}
